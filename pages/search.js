40610b2eeb7c0dab24f80c230d1b8f8373fe1e57
papa-react <makiya112545@gmail.com>
2021-12-17 20:47:43 -1200

    search component

---
+++ pages/search.js
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/router';
+import { format} from 'date-fns'
+import { fetcher } from '../Fetcher/Fetcher';
+import InfoCard from '../components/InfoCard';
+
+const Search = ({ searchResult }) => {
+    
+    const router = useRouter();
+    const { location, startDate, endDate, guests } = router.query;
+    const formattedStartDate = format(new Date(startDate), 'dd MMMM yyyy'); 
+    const formattedEndDate = format(new Date(endDate), 'dd MMMM yyyy');
+    const range = `${formattedStartDate} to ${formattedEndDate}`
+    return (
+        <main className=" min-h-[80vh] flex ">
+            <section className="pt-7 px-6 " >
+                <p className="text-sm select-none text-slate-600 " >300+ stays {range} for {guests} guests </p>
+                <h1 className="text-2xl font-section select-none mt-1 mb-3 text-slate-700 " >Stays in {location}</h1>
+                <div className="hidden sm:flex items-center gap-x-2 " >
+                    <p className="search-btn" >Cancellation Flexibility </p>
+                    <p className="search-btn" >Type of Place </p>
+                    <p className="search-btn" >Rooms and Beds </p>
+                    <p className="search-btn" >More filters </p>
+                </div>
+                <div className="flex flex-col gap-y-3 mt-4 py-4 mb-10 " >
+                    {
+                        searchResult.map((items) => (
+                            <InfoCard key={items.img} {...items} />
+                        ))
+                    }
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export async function getServerSideProps() {
+    const searchResult = await fetcher('https://links.papareact.com/isz')
+    return {
+        props: {
+            searchResult
+        }
+    }
+}  
+
+export default Search
