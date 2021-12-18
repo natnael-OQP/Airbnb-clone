d4516c675f00663643f417e8313b8e7507e4bc32
papa-react <makiya112545@gmail.com>
2021-12-17 20:46:41 -1200

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import Default from '../layout/Default'
 import '../styles/globals.css'
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const progress = new ProgressBar({
+    size: 2,
+    color: "#FE595E",  
+    className: "z-50",
+    delay: 100,
+  });
+
+  Router.events.on('routeChangeStart', () => progress.start());
+  Router.events.on('routeChangeComplete', () => progress.finish());
+
   return (
     <Default>
       <Component {...pageProps} />
