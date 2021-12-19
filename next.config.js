53d73d6c206571cda1656bdfd4cc3a56546d204b
papa-react <makiya112545@gmail.com>
2021-12-19 08:17:48 -1200

    add env variable

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
     formats: ['image/avif', 'image/webp'],
+  },
+  env: {
+    mapbox_key:'pk.eyJ1IjoibmF0bmFlbC1vcHEiLCJhIjoiY2t4YzY3eTZtMTRyczJxa3QzamJidG1qNCJ9.t6cGHvTM3ggTnq3XOIz9mQ'
   }
 }
